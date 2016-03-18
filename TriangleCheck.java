e8768eb30cc7ef54b9bd57e7e8dc0d7d536e1bf5
mihhazz <mihhazz@mail.ru>
2016-03-18 23:31:23 +0300

    Added files via upload

--- TriangleCheck.java
+++ TriangleCheck.java
@@ -1,14 +1,18 @@
 package triangle;
-import triangle.*;  // думаю что это лишнее
+
 public  class TriangleCheck {
-	
+public Point one;
+public Point two;
+public Point three;
+
 // проверка условий конструктором
 
- class TriangleCheck (Point pointOne, Point pointTwo, Point pointThree ) 
- {
+ //class TriangleCheck (Point pointOne, Point pointTwo, Point pointThree ) 
+ 
+	 
 
  
-if ( pointOne.x == pointOne.y && pointTwo.x == pointTwo.y && pointThree.x ==pointThree.y ||  pointOne.x == pointTwo.x && pointTwo.x == pointThree.x 
+/*if ( pointOne.x == pointOne.y && pointTwo.x == pointTwo.y && pointThree.x ==pointThree.y ||  pointOne.x == pointTwo.x && pointTwo.x == pointThree.x 
 ||  pointOne.y == pointTwo.y && pointTwo.y == pointThree.y  || pointOne.x == 0 && pointOne.y ==0 || 
  pointTwo.x == 0 && pointTwo.y ==0  || pointThree.x == 0 && pointThree.y ==0 ) {	
 	
@@ -18,5 +22,5 @@ if ( pointOne.x == pointOne.y && pointTwo.x == pointTwo.y && pointThree.x ==poin
  System.out.println("Прямая линия либо введены нулевые координаты! Это не треугольник.  ");
  
     }		
-   }
+   }*/
 }
