483c6a854d54751309d29f4fa78036918abd3d17
mihhazz <mihhazz@mail.ru>
2016-03-20 22:50:43 +0300

    laba 1.4

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+package factorials;
+
+public  class Factorial {
+	
+public int result= 1;
+
+public int calc(int factor) {
+	for (int i = 3 ; i<=factor; i++)  
+	return  result * i;	
+	
+	 
+	
+	
+	}	
+	
+	
+}
---
+++ Factorialinit.java
@@ -0,0 +1,17 @@
+package factorials;
+
+public class Factorialinit {
+	
+	
+	public static void main(String[] args) {
+	Factorial F = new Factorial(); 
+	int result;
+	result = F.calc(10);
+	
+	System.out.println("factorial = "  +result);	
+		
+	}
+	
+	
+	
+}
