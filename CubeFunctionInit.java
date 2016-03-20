f7b941bd7ca7e12eced8d58accdf20871d17b2c1
mihhazz <mihhazz@mail.ru>
2016-03-20 20:46:47 +0300

    Update CubeFunctionInit.java

--- CubeFunctionInit.java
+++ CubeFunctionInit.java
@@ -8,11 +8,9 @@ int x2=0;
 
 CubeFunction c = new CubeFunction(5,7,12);
   
- for(String arg : args) {
-	 
-	x1 = Integer.parseInt(args[0]);  
-    x2 = Integer.parseInt(args[1]);
- }
+ x1 = Integer.parseInt(args[0]);  
+ x2 = Integer.parseInt(args[1]);
+ 
 for (int i = x1; i<=x2; i++) {
 result = c.calculate(i);
 System.out.println("x = "+i +"  y = " + result );
